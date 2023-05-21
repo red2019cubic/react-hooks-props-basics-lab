db110fe8d7eca7944e0dcd3ac36903b745163663
Red <red.sarouf2014@gmail.com>
2023-05-21 09:39:35 -0400

    add props basics lab

---
+++ src/components/Links.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+
+function Links({github, linkedin}) {
+//const links = props.links;
+
+
+  return (
+<div>
+    <h3>Links</h3>
+    <a href={github}>{github}</a>
+    <a href={linkedin}>{linkedin}</a>
+</div>
+  );
+}
+
+export default Links;
